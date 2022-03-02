238b3e829b81cc45b700e1b7ce2b935579a1675c
Orevaoghene Ukah <oreva.ukah@yahoo.com>
2022-03-02 21:58:09 +0100

    add more changes

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch( 'http://localhost:3000/register', {
+    fetch( 'https://blooming-forest-94209.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
